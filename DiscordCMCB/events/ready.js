f9e24dcaafed70f30efa1d4f91d171118d394bbc
Triz <kiebusiness1@gmail.com>
2021-02-24 05:07:24 +0000

    Update ready.js

--- DiscordCMCB/events/ready.js
+++ DiscordCMCB/events/ready.js
@@ -3,8 +3,16 @@ import * as fs from 'fs';
 import * as defaults from '../datapull/defaults.js';
 import * as newStream from '../twitchStreams.js';
 
-export default function (client, message) {
+export default async function (client, message) {
     console.log(chalk.cyan.bold(`===> ${chalk.green.bold('READY!')} <===`))
+
+    function getGuildListToConsole() {
+        console.table(client.guilds.cache.reduce((acc, guild) => {
+            acc[guild.id] = guild.name
+            return acc
+        }, {}))
+    }
+    // getGuildListToConsole();
 
     // @@setPresence
     setInterval(() => {
@@ -28,15 +36,15 @@ export default function (client, message) {
     setInterval(function testLiveStream() {
         let liveStreamTest = false;
         if (liveStreamTest === true) {
-            newStream.liveStreamAnnouncements('domosplace', client.config.client_id, client.twitchTokens.accessToken, 'RED', '768289504603275265', '798753871353741312', '809620514867249183');
+            newStream.liveStreamAnnouncements(client, 'domosplace', client.config.client_id, client.twitchTokens.accessToken, '768289504603275265', '798753871353741312', '809620514867249183');
         }
     }, 1000 * testTimer);
-    // setInterval(function liveStreamInterval() {
-    //     if (liveStreamBool == true) {
-    //         client.newStream.liveStreamAnnouncements('itsjusttriz', client.config.client_id, client.twitchTokens.accessToken, 'PURPLE', '466402083466379267', '748288027322875976', '770312500951908432', '748288230545162280');
-    //         client.newStream.liveStreamAnnouncements('bmwhd1', client.config.client_id, client.twitchTokens.accessToken, 'PURPLE', '314737648839294986', '780914960401694741', '780915508425261056', '314737648839294986');
-    //         client.newStream.liveStreamAnnouncements('domosplace', client.config.client_id, client.twitchTokens.accessToken, 'PURPLE', '155849052850880513', '770293246215192617', '770293386942218270', '797241646454407178');
-    //         client.newStream.liveStreamAnnouncements('finncapp', client.config.client_id, client.twitchTokens.accessToken, 'PURPLE', '585627689612869645', '770296176565682266', '770302037169930300', '744403558522159144');
-    //     } else return;
-    // }, 1000 * liveStreamTimer);
+    setInterval(function liveStreamInterval() {
+        if (liveStreamBool === true) {
+            newStream.liveStreamAnnouncements(client, 'itsjusttriz', client.config.client_id, client.twitchTokens.accessToken, '466402083466379267', '748288027322875976', '770312500951908432', '748288230545162280');
+            newStream.liveStreamAnnouncements(client, 'bmwhd1', client.config.client_id, client.twitchTokens.accessToken, '314737648839294986', '780914960401694741', '780915508425261056');
+            newStream.liveStreamAnnouncements(client, 'domosplace', client.config.client_id, client.twitchTokens.accessToken, '155849052850880513', '770293246215192617', '770293386942218270', '797241646454407178');
+            newStream.liveStreamAnnouncements(client, 'finncapp', client.config.client_id, client.twitchTokens.accessToken, '585627689612869645', '770296176565682266', '770302037169930300', '744403558522159144');
+        } else return;
+    }, 1000 * liveStreamTimer);
 }
