00439fb609809872e2f68fc72d0487ece34d5b58
Triz <kiebusiness1@gmail.com>
2021-02-24 05:07:43 +0000

    Update events.js

--- DiscordCMCB/guilds/cmcb_official/events.js
+++ DiscordCMCB/guilds/cmcb_official/events.js
@@ -1,70 +1,100 @@
-import * as Discord from 'discord.js';
-import * as $ from '../../datapull/defaults.js';
-import * as config from '../../config.js';
-import { default as axios } from 'axios';
+import { MessageEmbed } from "discord.js";
+import chalk from 'chalk';
 
 // Channels
 const guildChannelMap = new Map([
     ['mainLogChannel', '768956331507580948'],
     ['roleLogChannel', '775541436577218571'],
-    ['joinLogChannel', '775541639703560192']
+    ['joinLogChannel', '775541639703560192'],
+    ['messageLogChannel', '775541523227738112']
 ])
 
 export async function handleGuildMemberUpdate(client, oldMember, newMember) {
+    let channel = newMember.guild.channels.cache.get(guildChannelMap.get('roleLogChannel'))
     // If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
+
     const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+
     if (removedRoles.size > 0) {
-        console.log(`The roles ${removedRoles.map(r => r.name)} were removed from ${oldMember.displayName} in ${oldMember.guild.name}.`);
-        let removedRolesEmbed = new Discord.MessageEmbed()
+        // console.log(`The roles ${removedRoles.map(r => r.name)} were removed from ${oldMember.displayName} in ${oldMember.guild.name}.`);
+        let embed1 = new MessageEmbed()
             .setAuthor(`${oldMember.user.tag}`, oldMember.user.displayAvatarURL())
-            .addField(":no_entry: Removed Role", `${removedRoles.map(r => r.name)}`)
+            .addField(`${client.systemEmojis.get('BACKEND_MINUS')} Removed Role`, `${removedRoles.map(r => r.name)}`)
             .setFooter(`UserID: ${oldMember.id}`)
+            .setColor(client.colorWheel.get('RED'))
             .setTimestamp()
-        await client.channels.fetch('775541436577218571').then(channel => {
-            channel.send(removedRolesEmbed)
-        })
+
+        return channel.send(embed1)
     }
+
     // If the role(s) are present on the new member object but are not on the old one (i.e role(s) were added)
     const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+
     if (addedRoles.size > 0) {
-        console.log(`The roles ${addedRoles.map(r => r.name)} were added to ${oldMember.displayName} in ${oldMember.guild.name}.`);
-        let addedRolesEmbed = new Discord.MessageEmbed()
+        // console.log(`The roles ${addedRoles.map(r => r.name)} were added to ${oldMember.displayName} in ${oldMember.guild.name}.`);
+        let embed2 = new MessageEmbed()
             .setAuthor(`${oldMember.user.tag}`, oldMember.user.displayAvatarURL())
-            .addField(":white_check_mark: Added Role", `${addedRoles.map(r => r.name)}`)
+            .addField(`${client.systemEmojis.get('BACKEND_PLUS')} Added Role`, `${addedRoles.map(r => r.name)}`)
             .setFooter(`UserID: ${oldMember.id}`)
+            .setColor(client.colorWheel.get('GREEN'))
             .setTimestamp()
-        await client.channels.fetch('775541436577218571').then(channel => {
-            channel.send(addedRolesEmbed)
-        })
+
+        return channel.send(embed2)
     }
 }
 
-export async function handleGuildMemberAdd(member) {
-    let memberJoinEmbed = new Discord.MessageEmbed()
-        .setColor('#42f572')
-        .addField('Action', '<:joinServer:770829534793826314> Member Joined', false)
+export async function handleGuildMemberAdd(client, member) {
+    let embed = new MessageEmbed()
+        .setColor(client.colorWheel.get('GREEN'))
+        .addField('Action', `${client.systemEmojis.get('BACKEND_JOIN')} Member Joined`, false)
         .addField('Username', member.user.username, true)
         .addField('ID', member.user.id, false)
         .addField('Bot?', member.user.bot, true)
         .addField('Discriminator', member.user.discriminator, true)
         .setThumbnail(member.user.displayAvatarURL())
         .setTimestamp()
-    await client.channels.fetch('775541639703560192').then(channel => {
-        channel.send(memberJoinEmbed)
+    await client.channels.fetch(guildChannelMap.get('joinLogChannel')).then(channel => {
+        return channel.send(embed)
     })
 }
 
-export async function handleGuildMemberRemove(member) {
-    let memberLeaveEmbed = new Discord.MessageEmbed()
-        .setColor('#ff000d')
-        .addField('Action', '<:leaveServer:770829554059182090> Member Left', false)
+export async function handleGuildMemberRemove(client, member) {
+    let embed = new MessageEmbed()
+        .setColor(client.colorWheel.get('RED'))
+        .addField('Action', `${client.systeEmojis.get('BACKEND_LEAVE')} Member Left`, false)
         .addField('Username', member.user.username, true)
         .addField('ID', member.user.id, false)
         .addField('Bot?', member.user.bot, true)
         .addField('Discriminator', member.user.discriminator, true)
         .setThumbnail(member.user.displayAvatarURL())
         .setTimestamp()
-    await client.channels.fetch('775541639703560192').then(channel => {
-        channel.send(memberLeaveEmbed)
+    await client.channels.fetch(guildChannelMap.get('joinLogChannel')).then(channel => {
+        return channel.send(embed)
     })
+}
+
+export async function handleMessageDelete(client, message) {
+
+    if (message.member.id === client.user.id || message.channel.id === guildChannelMap.get('mainLogChannel')) return;
+
+    let embed = new MessageEmbed()
+        .setColor(message.member.displayHexColor)
+        .setTimestamp()
+        .setAuthor(`${message.member.user.tag} (${message.member.user.id})`, message.member.user.displayAvatarURL())
+        .setThumbnail(message.member.user.displayAvatarURL())
+        .addField('❯ Message ID', message.id)
+        .addField('❯ Message Content', message.content)
+        .addField('❯ Channel Name', message.channel.name)
+        .addField('❯ Channel ID', message.channel.id)
+        .addField('❯ Action', 'Message Delete')
+    await client.channels.fetch(guildChannelMap.get('messageLogChannel')).then(channel => { return channel.send(embed) })
+}
+
+export async function handleVoiceStateUpdate(client, oldState, newState) {
+
+    const channel = oldState.guild.channels.cache.get(guildChannelMap.get('mainLogChannel'))
+
+    if (oldState.id === client.config.botOwnerId) {
+        console.log(chalk.green('voiceStateUpdate triggered! Find a logChannel for this.'))
+    }
 }
