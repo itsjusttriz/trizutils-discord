695fd075233b0865279d584ed15d87b60667c7c5
Triz <kiebusiness1@gmail.com>
2021-02-24 05:09:49 +0000

    Update rplist.js

--- TwitchCMCB/DataPull/rplist.js
+++ TwitchCMCB/DataPull/rplist.js
@@ -1,13 +1,4 @@
-const listpacks = ['wyld116, soartexfanver, soartexinvictus'];
-
-const soartexfanver = ['The current ResourcePack is Soartex Fanver. Find out more info here - https://soartex.net/'];
-const soartexinvictus = ['The current ResourcePack is Soartex Invictus. Find out more info here - https://soartex.net/'];
-const wyld116 = ['The current ResourcePack is Wyld116, a custom resourcepack developed by Wyld. This resourcepack is only available to subscribers of Wyld and LadyHana.'];
-
-
-module.exports.listpacks = listpacks;
-
-
-module.exports.soartexfanver = soartexfanver;
-module.exports.soartexinvictus = soartexinvictus;
-module.exports.wyld116 = wyld116;
+export const listpacks = 'wyld116, soartexfanver, soartexinvictus';
+export const soartexfanver = 'The current ResourcePack is Soartex Fanver. Find out more info here - https://soartex.net/';
+export const soartexinvictus = 'The current ResourcePack is Soartex Invictus. Find out more info here - https://soartex.net/';
+export const wyld116 = 'The current ResourcePack is Wyld116, a custom resourcepack developed by Wyld. This resourcepack is only available to subscribers of Wyld and LadyHana.';
