5fc304847f181d4e00aa4f647ec13b183667ea04
Triz <kiebusiness1@gmail.com>
2021-02-24 05:05:48 +0000

    Update kick.js

--- DiscordCMCB/commands/kick.js
+++ DiscordCMCB/commands/kick.js
@@ -1,21 +1,16 @@
 export default {
     name: "Kick",
-    perms: 'Mod only',
     usage: 'n!kick <user-id> OR n!kick <user-mention>',
     description: 'Kicks a targetted member',
+    permissions: ['KICK_MEMBERS'],
+    hidden: false,
     run(client, message, args = [mention, ...reason]) {
-        const modRole = message.guild.roles.cache.find(r => r.name.match(/mod/gi));
-        if (!modRole)
-            return message.reply('Cannot find moderator role. Please report this!')
+        message.delete({ timeout: 3000 });
 
-        if (!message.member.roles.cache.has(modRole.id))
-            return message.reply('You do not have the required role, for this command.')
+        if (message.author.id !== client.config.botOwnerId && this.permissions.indexOf(message.member.permissions) < 0) return message.reply(`You must have one of the following permissions to use this command: \`${this.permissions.join(' | ')}\``).then(m => m.delete({ timeout: 1000 * 10 }))
 
         if (message.mentions.members.size === 0)
             return message.reply('Please mention a user to kick.');
-
-        if (!message.guild.me.hasPermission("KICK_MEMBERS"))
-            return message.reply('You must have the KICK_MEMBERS permission to use this command.');
 
         const target = message.mentions.members.first() || message.guild.members.cache.get(mention);
 
