f348756f35295d256b6dfc1895b3869deb153907
Triz <kiebusiness1@gmail.com>
2021-02-24 05:06:54 +0000

    Update guildMemberAdd.js

--- DiscordCMCB/events/guildMemberAdd.js
+++ DiscordCMCB/events/guildMemberAdd.js
@@ -1,10 +1,10 @@
-import { default as guildFiles } from '../datapull/guildFileImports.js';
+import guildFileImports from '../datapull/guildFileImports.js';
 
 export default async function (client, member) {
     if (member.guild.id === client.guildIdList.get('nightshade_alley')) {
-        (await guildFiles.NIGHTSHADE_ALLEY).handleGuildMemberAdd(client, member);
+        (await guildFileImports.NIGHTSHADE_ALLEY).handleGuildMemberAdd(client, member);
     } else if (member.guild.id === client.guildIdList.get('cmcb_official')) {
-        (await guildFiles.CMCB_OFFICIAL).handleGuildMemberAdd(client, member);
+        (await guildFileImports.CMCB_OFFICIAL).handleGuildMemberAdd(client, member);
     } else
         return;
 }
