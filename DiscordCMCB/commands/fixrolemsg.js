b4c1fc8abb530124c6243cdd6c86e9d000192df4
Triz <kiebusiness1@gmail.com>
2021-03-05 04:54:44 +0000

    Update fixrolemsg.js

--- DiscordCMCB/commands/fixrolemsg.js
+++ DiscordCMCB/commands/fixrolemsg.js
@@ -1,4 +1,5 @@
 import { MessageEmbed } from 'discord.js';
+import { default as config } from '../config.js';
 
 const idStorage = [
     {
@@ -19,16 +20,22 @@ const idStorage = [
 
 export default {
     name: 'fixrolemsg',
-    usage: 'n!fixrolemsg <option> (Possible Options: `general`, `games`, `optional`)',
+    usage: `n!fixrolemsg <option>`,
+    requiredArgs: [],
     description: 'Fixes an embed in various guilds.',
     permissions: '@botOwner',
     hidden: true,
     run(client, message, args) {
         message.delete({ timeout: 5000 });
 
-        if (message.author.id !== client.config.botOwnerId) return;
+        if (message.author.id !== config.botOwnerId) return;
 
-        if (message.channel.id === idStorage[0].channelIds[0].channelId ?? message.channel.id === idStorage[0].channelIds[1].channelId) {
+        if (message.channel.id === idStorage[0].channelIds[0].channelId || message.channel.id === idStorage[0].channelIds[1].channelId) {
+            this.requiredArgs = ['general', 'games', 'optional'];
+
+            let roleChan = message.guild.channels.cache.get(idStorage[0].channelIds[0].channelId);
+
+            if (!args[0] || this.requiredArgs.indexOf(args[0]) < 0) return message.reply(`Please specify an option. \`\`\`\n${this.usage} (Possible Options: ${this.requiredArgs?.join(', ')})\n\`\`\``).then(msg => msg.delete({ timeout: 1000 * 10 })).catch(console.error);
 
             switch (args[0]) {
                 case 'general':
@@ -42,26 +49,25 @@ export default {
                         .addField('LIVE', 'Users with this role are currently in Streamer Mode and you should go show them some love. :wink:')
                         .addField('Mods', `Users with this role hold the hammer. Follow the rules and they won't "accidentally" drop it on your head. :smiling_imp:`)
                         .addField('Admin', `Users with this role maintain the "behind-the-scenes" functions of both this server and Finn's Twitch.`)
-                    message.guild.channels.cache.get(idStorage[0].channelIds[0].channelId)
-                        .messages.fetch('771467879118798888')
-                        .then(m => m.edit(roleMsg1));
+                    roleChan.messages.fetch('771467879118798888')
+                        .then(m => m.edit(roleMsg1)).catch(console.error);
                     break;
                 case 'games':
                     let roleMsg2 = new MessageEmbed()
                         .setTitle('Games')
                         .setColor('#066A00')
                         .setDescription(`-----------------------------------------------------------------------------------------------\n-----------------------------------------------------------------------------------------------`)
-                        .addField(':joystick: Games - General', `Users with this role will gain access to the <#586461487221178378>. This channel is used for all games that **DON'T** have their own channel.`)
-                        .addField(':sauropod: Games - Ark', 'Users with this role will gain access to Ark-related text channels & voice channels.')
-                        .addField(':smiling_imp:  Games - Diablo', 'Users with this role will gain access to Diablo-related text channels & voice channels.')
-                        .addField(':magic_wand: Games - Hades', 'Users with this role will gain access to Hades-related text channels & voice channels.')
-                        .addField(':pick: Games - Minecraft', 'Users with this role will gain access to Minecraft-related text channels & voice channels.')
-                        .addField(':crystal_ball: Games - Path of Exile', 'Users with this role will gain access to POE-related text channels & voice channels.')
-                        .addField(':crossed_swords: Games - World of Warcraft', 'Users with this role will gain access to World of Warcraft-related text channels & voice channels.')
-                        .addField(':octagonal_sign: Games - Community Servers', 'Users with this role will gain access to the Community <#676294700402999296> channel.\n**THIS ROLE IS NOT CLAIMABLE**')
-                    message.guild.channels.cache.get(idStorage[0].channelIds[0].channelId)
-                        .messages.fetch('771467880021098544')
-                        .then(m => m.edit(roleMsg2));
+                        .addField('🕹️ Games - General', `Users with this role will gain access to the <#586461487221178378>. This channel is used for all games that **DON'T** have their own channel.`)
+                        .addField('🦕 Games - Ark', 'Users with this role will gain access to Ark-related text channels & voice channels.')
+                        .addField('😈  Games - Diablo', 'Users with this role will gain access to Diablo-related text channels & voice channels.')
+                        .addField('🪄 Games - Hades', 'Users with this role will gain access to Hades-related text channels & voice channels.')
+                        .addField('⛏️ Games - Minecraft', 'Users with this role will gain access to Minecraft-related text channels & voice channels.')
+                        .addField('🔮 Games - Path of Exile', 'Users with this role will gain access to POE-related text channels & voice channels.')
+                        .addField('⚔️ Games - World of Warcraft', 'Users with this role will gain access to World of Warcraft-related text channels & voice channels.')
+                        .addField('🏕️ Games - Valheim', 'Users with this role will gain access to the Valheim-related text & voice channels.')
+                        .addField('🛑 Games - Community Servers', 'Users with this role will gain access to the Community <#676294700402999296> channel.\n**THIS ROLE IS NOT CLAIMABLE**')
+                    roleChan.messages.fetch('771467880021098544')
+                        .then(m => m.edit(roleMsg2)).catch(console.error);;
                     break;
                 case 'optional':
                     let roleMsg3 = new MessageEmbed()
@@ -70,14 +76,13 @@ export default {
                         .setDescription(`-----------------------------------------------------------------------------------------------\n-----------------------------------------------------------------------------------------------`)
                         .addField(':mega: FinnStream - Notify', 'Users with this role will be pinged when <@!763135917329743903> posts that Finn is currently streaming!')
                         .setTimestamp()
-                    message.guild.channels.cache.get(idStorage[0].channelIds[0].channelId)
-                        .messages.fetch('771467881086976011')
-                        .then(m => m.edit(roleMsg3))
+                    roleChan.messages.fetch('771467881086976011')
+                        .then(m => m.edit(roleMsg3)).catch(console.error);
                     break;
                 default:
-                    message.reply(`Please specify an option. (*${this.usage}*)`).then(msg => msg.delete({ timeout: 1000 * 10 }))
+                    message.reply(`Please specify an option. \`\`\`\n${this.usage} (Possible Options: ${this.requiredArgs?.join(', ')})\n\`\`\``).then(msg => msg.delete({ timeout: 1000 * 10 })).catch(console.error);
                     break;
             }
-        } else message.reply('This command is disabled here.').then(m => m.delete({ timeout: 3000 }))
+        } else message.reply('This command is disabled here.').then(m => m.delete({ timeout: 3000 })).catch(console.error);
     }
 }
