6c552d996753aed22271d11c051751a32ffe5415
Triz <kiebusiness1@gmail.com>
2021-03-05 05:03:42 +0000

    Update ready.js

--- DiscordCMCB/events/ready.js
+++ DiscordCMCB/events/ready.js
@@ -6,13 +6,12 @@ import * as newStream from '../twitchStreams.js';
 export default async function (client, message) {
     console.log(chalk.cyan.bold(`===> ${chalk.green.bold('READY!')} <===`))
 
-    function getGuildListToConsole() {
-        console.table(client.guilds.cache.reduce((acc, guild) => {
-            acc[guild.id] = guild.name
-            return acc
-        }, {}))
-    }
-    // getGuildListToConsole();
+    // function getGuildListToConsole(client) {
+    //     console.table(client.guilds.cache.reduce((acc, guild) => {
+    //         acc[guild.id] = guild.name
+    //         return acc
+    //     }, {}))
+    // }
 
     // @@setPresence
     setInterval(() => {
@@ -20,31 +19,34 @@ export default async function (client, message) {
             // .then(console.log)
             .catch(console.error);
     }, 1000 * 60);
+
     // @@GrabTwitchToken
     fs.readFile('./twitch-tokens.json', (err, data) => {
         client.twitchTokens = JSON.parse(data);
     });
-    setInterval(async function tokenGrabInterval() {
+
+    setInterval(async () => {
         fs.readFile('./twitch-tokens.json', (err, data) => {
             client.twitchTokens = JSON.parse(data);
         })
     }, 1000 * 60 * 3);
+
     // @@Live-Announcements
     let liveStreamBool = true;
-    let liveStreamTimer = 60 * 5;
-    let testTimer = 5;
-    setInterval(function testLiveStream() {
-        let liveStreamTest = false;
-        if (liveStreamTest === true) {
-            newStream.liveStreamAnnouncements(client, 'domosplace', client.config.client_id, client.twitchTokens.accessToken, '768289504603275265', '798753871353741312', '809620514867249183');
-        }
-    }, 1000 * testTimer);
-    setInterval(function liveStreamInterval() {
-        if (liveStreamBool === true) {
-            newStream.liveStreamAnnouncements(client, 'itsjusttriz', client.config.client_id, client.twitchTokens.accessToken, '466402083466379267', '748288027322875976', '770312500951908432', '748288230545162280');
-            newStream.liveStreamAnnouncements(client, 'bmwhd1', client.config.client_id, client.twitchTokens.accessToken, '314737648839294986', '780914960401694741', '780915508425261056');
-            newStream.liveStreamAnnouncements(client, 'domosplace', client.config.client_id, client.twitchTokens.accessToken, '155849052850880513', '770293246215192617', '770293386942218270', '797241646454407178');
-            newStream.liveStreamAnnouncements(client, 'finncapp', client.config.client_id, client.twitchTokens.accessToken, '585627689612869645', '770296176565682266', '770302037169930300', '744403558522159144');
-        } else return;
-    }, 1000 * liveStreamTimer);
+    let liveStreamTestBool = false;
+
+    // Testing...
+    setInterval(() => {
+        if (liveStreamTestBool !== true) return;
+        newStream.liveStreamAnnouncements(client, 'domosplace', client.config.client_id, client.twitchTokens.accessToken, '768289504603275265', '798753871353741312', '809620514867249183');
+    }, 1000 * 5);
+
+    // Real-Deal...
+    setInterval(() => {
+        if (liveStreamBool !== true) return;
+        newStream.liveStreamAnnouncements(client, 'itsjusttriz', client.config.client_id, client.twitchTokens.accessToken, '466402083466379267', '748288027322875976', '770312500951908432', '748288230545162280');
+        newStream.liveStreamAnnouncements(client, 'bmwhd1', client.config.client_id, client.twitchTokens.accessToken, '314737648839294986', '780914960401694741', '780915508425261056');
+        newStream.liveStreamAnnouncements(client, 'domosplace', client.config.client_id, client.twitchTokens.accessToken, '155849052850880513', '770293246215192617', '770293386942218270', '797241646454407178');
+        newStream.liveStreamAnnouncements(client, 'finncapp', client.config.client_id, client.twitchTokens.accessToken, '585627689612869645', '770296176565682266', '770302037169930300', '744403558522159144');
+    }, 1000 * 60 * 5);
 }
