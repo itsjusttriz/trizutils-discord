07296dbc81effa2f4a280f4175f27bc1848777c7
Triz <kiebusiness1@gmail.com>
2021-03-07 06:16:37 +0000

    Added TimerManager#Import() to 'onRegister' eventhandler

--- TwitchCMCB/events/onRegister.js
+++ TwitchCMCB/events/onRegister.js
@@ -4,21 +4,42 @@ import { DeathsManager } from '../utils/DeathsManager.js';
 import { MoSManager } from "../utils/MoSManager.js";
 import { ToolsManager } from "../utils/ToolsManager.js";
 import { CooldownManager } from '../utils/CooldownManager.js';
+import { TimerManager, TimerRunner } from '../utils/TimerManager.js';
+import { default as tokenData } from "../tokens.json";
 
 export default async function (chatClient, apiClient) {
-    console.log(chalk.cyan.bold(`===> ${chalk.green.bold('READY!')} <===`))
+    console.log(chalk.cyan.bold(`===> ${chalk.green.bold('READY!')} <===`));
+
+    const nonTurboColors = {
+        colorList: ['blue', 'blueviolet', 'cadetblue', 'chocolate', 'coral', 'dodgerblue', 'firebrick', 'goldenrod', 'green', 'hotpink', 'orangered', 'red', 'seagreen', 'springgreen', 'yellowgreen'],
+        random() {
+            return this.colorList[Math.floor((Math.random() * this.colorList.length))];
+        }
+    }
+
+    setInterval(function () {
+        chatClient.changeColor(nonTurboColors.random());
+    }, 1000 * 2);
 
     ChannelManager.import(chatClient);
     DeathsManager.import();
     MoSManager.import();
     ToolsManager.import();
     CooldownManager.import();
+    TimerManager.import();
+
+    for (let x in TimerRunner) {
+        for (let y in TimerRunner[x]) {
+            TimerRunner[x][y](chatClient, apiClient);
+        }
+    }
 
     const streamInfo = await apiClient.helix.streams.getStreamByUserName('itsjusttriz');
 
     setInterval(async () => {
         if (streamInfo) {
-            console.log('');
+            console.log('Triz Live!');
         }
-    }, 1000 * 60 * 1);
+    }, 1000 * 1);
+    console.log(tokenData)
 }
