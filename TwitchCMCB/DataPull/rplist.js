5311da082055881075d6553c04bdfda1524f87fe
Triz <kiebusiness1@gmail.com>
2021-03-07 06:09:33 +0000

    Removed subdir 'DataPull'

--- TwitchCMCB/DataPull/rplist.js
+++
@@ -1,4 +0,0 @@
-export const listpacks = 'wyld116, soartexfanver, soartexinvictus';
-export const soartexfanver = 'The current ResourcePack is Soartex Fanver. Find out more info here - https://soartex.net/';
-export const soartexinvictus = 'The current ResourcePack is Soartex Invictus. Find out more info here - https://soartex.net/';
-export const wyld116 = 'The current ResourcePack is Wyld116, a custom resourcepack developed by Wyld. This resourcepack is only available to subscribers of Wyld and LadyHana.';
