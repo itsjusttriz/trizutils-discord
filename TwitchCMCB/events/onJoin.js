08eff0cfcf4bfaf92eb6b4b460aeeb9fb6d6dfe2
Triz <kiebusiness1@gmail.com>
2021-03-07 06:15:09 +0000

    Update onJoin.js

--- TwitchCMCB/events/onJoin.js
+++ TwitchCMCB/events/onJoin.js
@@ -3,6 +3,6 @@ import chalk from 'chalk';
 export default function (chatClient, channel, user) {
     if (user === 'nottriz') {
         console.log(chalk.green(`===> Joined ${channel} <===`));
-        chatClient.action('nottriz', `TESTING joined ${channel}`);
+        chatClient.action('nottriz', `joined ${channel}`);
     }
 }
