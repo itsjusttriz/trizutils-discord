1cc15893a3c323f0fe054d26bd48cc3c32ae9d96
Triz <kiebusiness1@gmail.com>
2021-03-08 09:43:58 +0000

    Updated redirect for issues.js

--- TwitchCMCB/commands/issues.js
+++ TwitchCMCB/commands/issues.js
@@ -3,7 +3,7 @@ export default {
     usage: 'n!issues',
     run(chatClient, message, args, options) {
 
-        chatClient.say(options.channel, 'View the current Issue Tracker for the bot here -> https://itsjusttriz.github.io/rd/issues/');
+        chatClient.say(options.channel, 'View the current Issue Tracker for the bot here -> http://triz.link/@mscb/github/issues/');
 
         return chatClient.say(options.logChan, options.logMsg);
     }
