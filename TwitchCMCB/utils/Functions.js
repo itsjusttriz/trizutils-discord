7349603ae347602f3a2e0484f06d58c861187cc7
Triz <kiebusiness1@gmail.com>
2021-03-09 06:17:12 +0000

    redefined various variables

--- TwitchCMCB/utils/Functions.js
+++ TwitchCMCB/utils/Functions.js
@@ -34,112 +34,116 @@ export function BlacklistedTerms(chatClient, channel, user, message, msg) {
     }
 }
 
-export function ExtraCommands(chatClient, channel, user, message, msg) {
+export async function ExtraCommands(options) {
 
-    const [exCommand, ...exArgs] = message.split(' ');
+    const [exCommand, ...exArgs] = options.message.split(' ');
 
     switch (exCommand.toLowerCase()) {
         case '!joinrace':
-            if (channel === '#reninsane') {
+            if (options.channel === '#reninsane') {
                 if (!options.isModPlus && !options.isBotAdmin) return;
 
-                if (CooldownManager.get(exCommand, channel) == true) return;
+                if (CooldownManager.get(exCommand, options.channel) == true) return;
 
-                chatClient.say(channel, '!joinrace');
+                options.chatClient.say(options.channel, '!joinrace');
 
-                CooldownManager.set(exCommand, channel, true);
+                CooldownManager.set(exCommand, options.channel, true);
                 setTimeout(() => {
                     CooldownManager.clear(exCommand, channel);
-                    chatClient.say(options.logChan, `${exCommand}'s cooldown has ended in ${channel}.`);
+                    options.chatClient.say(options.logChan, `${exCommand}'s cooldown has ended in ${options.channel}.`);
                 }, 1000 * 30);
 
-                return chatClient.say(options.logChan, options.logMsg);
+                return options.chatClient.say(options.logChan, options.logMsg);
             }
             break;
         case '!play':
-            if (channel === '#zeroxfusionz') {
+            if (options.channel === '#zeroxfusionz') {
                 if (!options.isBotAdmin) return;
 
-                if (CooldownManager.get(exCommand, channel) == true) return chatClient.say(channel, 'Cooldown is active.');
+                if (CooldownManager.get(exCommand, options.channel) == true) return options.chatClient.say(options.channel, 'Cooldown is active.');
 
-                chatClient.say(options.channel, `!play ${randomInt(1, 8)}`);
+                options.chatClient.say(options.channel, `!play ${randomInt(1, 8)}`);
 
-                CooldownManager.set(exCommand, channel, true);
+                CooldownManager.set(exCommand, options.channel, true);
                 setTimeout(() => {
                     CooldownManager.clear(exCommand, channel);
-                    chatClient.say(options.logChan, `${exCommand}'s cooldown has ended in ${channel}.`);
+                    options.chatClient.say(options.logChan, `${exCommand}'s cooldown has ended in ${options.channel}.`);
                 }, 1000 * 60 * 3);
 
-                return chatClient.say(options.logChan, options.logMsg);
+                return options.chatClient.say(options.logChan, options.logMsg);
             }
             break;
         case '!raid':
-            if (channel === '#immp') {
+            if (options.channel === '#immp') {
                 if (!options.isCaster && !options.isBotAdmin) return;
 
-                if (CooldownManager.get(exCommand, channel) == true) return chatClient.say(channel, 'Cooldown is active.');
+                if (CooldownManager.get(exCommand, options.channel) == true) return options.chatClient.say(options.channel, 'Cooldown is active.');
 
-                if (!exArgs[0]) return chatClient.say(channel, `${user}, you must specify a raid target! (e.g. !raid ${channel.substr(1)})`);
+                if (!exArgs[0]) return options.chatClient.say(options.channel, `${options.user}, you must specify a raid target! (e.g. !raid ${options.channel.substr(1)})`);
 
-                chatClient.action(channel, 'You have been Immplicated!! #ImmpRaid');
-                chatClient.action(channel, 'You have been Immplicated!! #ImmpRaid');
-                chatClient.action(channel, 'You have been Immplicated!! #ImmpRaid');
-                chatClient.say(channel, `/raid ${exArgs[0]}`);
+                options.chatClient.action(options.channel, 'You have been Immplicated!! #ImmpRaid');
+                options.chatClient.action(options.channel, 'You have been Immplicated!! #ImmpRaid');
+                options.chatClient.action(options.channel, 'You have been Immplicated!! #ImmpRaid');
+                options.chatClient.say(options.channel, `/raid ${exArgs[0]}`);
 
-                CooldownManager.set(exCommand, channel, true);
+                CooldownManager.set(exCommand, options.channel, true);
                 setTimeout(() => {
-                    CooldownManager.clear(exCommand, channel);
-                    chatClient.say(options.logChan, `${exCommand}'s cooldown has ended in ${channel}.`);
+                    CooldownManager.clear(exCommand, options.channel);
+                    options.chatClient.say(options.logChan, `${exCommand}'s cooldown has ended in ${options.channel}.`);
                 }, 1000 * 2);
 
-                return chatClient.say(options.logChan, options.logMsg);
-            } else if (channel === '#ja_keeler') {
+                return options.chatClient.say(options.logChan, options.logMsg);
+            } else if (options.channel === '#ja_keeler') {
                 if (!options.isCaster && !options.isBotAdmin) return;
 
-                if (CooldownManager.get(exCommand, channel) == true) return chatClient.say(channel, 'Cooldown is active.');
+                if (CooldownManager.get(exCommand, options.channel) == true) return options.chatClient.say(options.channel, 'Cooldown is active.');
 
-                if (!exArgs[0]) return chatClient.say(channel, `${user}, you must specify a raid target! (e.g. !raid ${channel.substr(1)})`);
+                if (!exArgs[0]) return options.chatClient.say(options.channel, `${options.user}, you must specify a raid target! (e.g. !raid ${options.channel.substr(1)})`);
 
-                chatClient.action(channel, 'Ja_Keeler, He makes you Happy!');
-                chatClient.action(channel, 'Ja_Keeler, He makes you Happy!');
-                chatClient.action(channel, 'Ja_Keeler, He makes you Happy!');
-                chatClient.say(channel, `/raid ${exArgs[0]}`);
+                options.chatClient.action(options.channel, 'Ja_Keeler, He makes you Happy!');
+                options.chatClient.action(options.channel, 'Ja_Keeler, He makes you Happy!');
+                options.chatClient.action(options.channel, 'Ja_Keeler, He makes you Happy!');
+                options.chatClient.say(options.channel, `/raid ${exArgs[0]}`);
 
-                CooldownManager.set(exCommand, channel, true);
+                CooldownManager.set(exCommand, options.channel, true);
                 setTimeout(() => {
-                    CooldownManager.clear(exCommand, channel);
-                    chatClient.say(options.logChan, `${exCommand}'s cooldown has ended in ${channel}.`);
+                    CooldownManager.clear(exCommand, options.channel);
+                    options.chatClient.say(options.logChan, `${exCommand}'s cooldown has ended in ${options.channel}.`);
                 }, 1000 * 2);
 
-                return chatClient.say(options.logChan, options.logMsg);
-            } else if (channel === '#reninsane') {
+                return options.chatClient.say(options.logChan, options.logMsg);
+            } else if (options.channel === '#reninsane') {
                 if (!options.isCaster && !options.isBotAdmin) return;
 
-                if (CooldownManager.get(exCommand, channel) == true) return chatClient.say(channel, 'Cooldown is active.');
+                if (CooldownManager.get(exCommand, options.channel) == true) return options.chatClient.say(options.channel, 'Cooldown is active.');
 
-                if (!exArgs[0]) return chatClient.say(channel, `${user}, you must specify a raid target! (e.g. !raid ${channel.substr(1)})`);
+                if (!exArgs[0]) return chatClient.say(options.channel, `${options.user}, you must specify a raid target! (e.g. !raid ${options.channel.substr(1)})`);
 
-                chatClient.action(channel, 'Reninsane welcomes you to the madhouse!')
-                chatClient.action(channel, 'Reninsane welcomes you to the madhouse!')
-                chatClient.action(channel, 'Reninsane welcomes you to the madhouse!')
-                chatClient.say(channel, `/raid ${exArgs[0]}`);
+                options.chatClient.action(options.channel, 'Reninsane welcomes you to the madhouse!')
+                options.chatClient.action(options.channel, 'Reninsane welcomes you to the madhouse!')
+                options.chatClient.action(options.channel, 'Reninsane welcomes you to the madhouse!')
+                options.chatClient.say(options.channel, `/raid ${exArgs[0]}`);
 
-                CooldownManager.set(exCommand, channel, true);
+                CooldownManager.set(exCommand, options.channel, true);
                 setTimeout(() => {
-                    CooldownManager.clear(exCommand, channel);
-                    chatClient.say(options.logChan, `${exCommand}'s cooldown has ended in ${channel}.`);
+                    CooldownManager.clear(exCommand, options.channel);
+                    options.chatClient.say(options.logChan, `${exCommand}'s cooldown has ended in ${options.channel}.`);
                 }, 1000 * 2);
 
-                return chatClient.say(options.logChan, options.logMsg);
+                return options.chatClient.say(options.logChan, options.logMsg);
             }
             break;
         case '!speak':
-            if (channel === '#matrixis') {
+            if (options.channel === '#matrixis') {
                 if (options.isModPlus) return;
 
-                chatClient.deleteMessage(channel, msg);
+                options.chatClient.deleteMessage(options.channel, options.msg);
 
-                return chatClient.say(options.logChan, options.logMsg);
+                return options.chatClient.say(options.logChan, options.logMsg);
             }
+            break;
+        case '!hearts':
+            await options.chatClient.commands.get('hearts').default.run(options.chatClient, options.message, options.args, options);
+            break;
     }
 }
