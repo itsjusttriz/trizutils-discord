6b277af4846f04775b4748ea48c0d92586505daf
Triz <kiebusiness1@gmail.com>
2021-04-01 03:58:33 +0100

    Fixed permission checking.

--- DiscordCMCB/commands/serverinfo.js
+++ DiscordCMCB/commands/serverinfo.js
@@ -10,19 +10,19 @@ export default {
     run(client, message, args) {
         message.delete({ timeout: 5000 })
 
-        if (message.author.id !== client.config.botOwnerId && this.permissions.indexOf(message.member.permissions) < 0) return message.reply(`You must have one of the following permissions to use this command: \`${this.permissions.join(' | ')}\``).then(m => m.delete({ timeout: 1000 * 10 }))
+        if (message.author.id !== client.config.botOwnerId && !message.member.permissions.has(this.permissions)) return message.reply(`You must have one of the following permissions to use this command: \`${this.permissions.join(' | ')}\``).then(m => m.delete({ timeout: 1000 * 10 }))
 
-        let serverTitle = message.content.match(/-name "([^"]*)"/)
-        let serverStatus = message.content.match(/-status "([^"]*)"/)
-        let serverIP = message.content.match(/-ip "([^"]*)"/)
-        let serverPort = message.content.match(/-port "([^"]*)"/)
-        let serverPassword = message.content.match(/-password "([^"]*)"/)
-        let serverGameVersion = message.content.match(/-gameversion "([^"]*)"/)
-        let serverMPVersion = message.content.match(/-mpversion "([^"]*)"/)
-        let serverRules = message.content.match(/-rules "([^"]*)"/)
-        let serverDownload = message.content.match(/-download "([^"]*)"/)
-        let serverIssueTracker = message.content.match(/-issues "([^"]*)"/)
-        let serverGuidance = message.content.match(/-guidance "([^"]*)"/)
+        let Title = message.content.match(/-name "([^"]*)"/)
+        let Status = message.content.match(/-status "([^"]*)"/)
+        let IP = message.content.match(/-ip "([^"]*)"/)
+        let Port = message.content.match(/-port "([^"]*)"/)
+        let Password = message.content.match(/-password "([^"]*)"/)
+        let GameVersion = message.content.match(/-gameversion "([^"]*)"/)
+        let MPVersion = message.content.match(/-mpversion "([^"]*)"/)
+        let Rules = message.content.match(/-rules "([^"]*)"/)
+        let Download = message.content.match(/-download "([^"]*)"/)
+        let IssueTracker = message.content.match(/-issues "([^"]*)"/)
+        let Guidance = message.content.match(/-guidance "([^"]*)"/)
 
         if (!args[0]) return message.reply(`Missing parameters: \`${this.requiredArgs}\` (*<> is required, [] is optional*).`);
 
@@ -31,48 +31,48 @@ export default {
             .setTimestamp()
             .setColor(message.member.displayHexColor ?? '#FEFEFE')
 
-        if (serverTitle) {
-            embed.setTitle(serverTitle?.[1].toString())
+        if (Title) {
+            embed.setTitle(Title?.[1].toString())
         }
-        if (serverStatus) {
-            embed.addField('Status', serverStatus?.[1].toString(), true)
-        }
-
-        if (serverIP) {
-            embed.addField('IP', serverIP?.[1].toString(), true)
+        if (Status) {
+            embed.addField('Status', Status?.[1].toString(), true)
         }
 
-        if (serverPort) {
-            embed.addField('Port', serverPort?.[1].toString(), true)
+        if (IP) {
+            embed.addField('IP', IP?.[1].toString(), true)
         }
 
-        if (serverPassword) {
-            embed.addField('Password', serverPassword?.[1].toString(), true)
+        if (Port) {
+            embed.addField('Port', Port?.[1].toString(), true)
         }
 
-        if (serverGameVersion) {
-            embed.addField('Game Version', serverGameVersion?.[1].toString(), true)
+        if (Password) {
+            embed.addField('Password', Password?.[1].toString(), true)
         }
 
-        if (serverMPVersion) {
-            embed.addField('Modpack Version', serverMPVersion?.[1].toString(), true)
+        if (GameVersion) {
+            embed.addField('Game Version', GameVersion?.[1].toString(), true)
         }
 
-        if (serverRules) {
-            embed.addField('Notes // Rules', serverRules?.[1].toString(), false)
+        if (MPVersion) {
+            embed.addField('Modpack Version', MPVersion?.[1].toString(), true)
         }
 
-        if (serverDownload) {
-            embed.addField('Download', `[Click Here](${serverDownload?.[1].toString()})`, true)
+        if (Rules) {
+            embed.addField('Notes // Rules', Rules?.[1].toString(), false)
         }
 
-        if (serverIssueTracker) {
-            embed.addField('Issue Tracker', `[Click Here](${serverIssueTracker?.[1].toString()})`, true)
+        if (Download) {
+            embed.addField('Download', `[Click Here](${Download?.[1].toString()})`, true)
         }
 
-        if (serverGuidance) {
-            embed.addField('Guidance', `[Click Here](${serverGuidance?.[1].toString()})`, true)
+        if (IssueTracker) {
+            embed.addField('Issue Tracker', `[Click Here](${IssueTracker?.[1].toString()})`, true)
         }
-        return message.reply(`creating embed... your message will be deleted in 5 seconds...`).then(m => setTimeout(() => m.edit('', embed), 1000 * 3))
+
+        if (Guidance) {
+            embed.addField('Guidance', `[Click Here](${Guidance?.[1].toString()})`, true)
+        }
+        return message.reply(`Creating Embed... Your message will be deleted in 5 seconds.`).then(m => setTimeout(() => m.edit('', embed), 1000 * 3))
     }
 }
