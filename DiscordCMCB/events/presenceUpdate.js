5e8924c9e0ee159d2ddc2322311f2498a66cd834
Triz <kiebusiness1@gmail.com>
2021-04-07 07:36:41 +0100

    Added guilds to the presenceUpdate listener

--- DiscordCMCB/events/presenceUpdate.js
+++ DiscordCMCB/events/presenceUpdate.js
@@ -4,5 +4,9 @@ export default async function (client, oldPresence, newPresence) {
 
     if (newPresence.guild.id === client.guildIdList.get('nightshade_alley')) {
         (await guildFileImports.NIGHTSHADE_ALLEY).handlePresenceUpdate(client, oldPresence, newPresence);
+    } else if (newPresence.guild.id === client.guildIdList.get('blitzyuk')) {
+        (await guildFileImports.BLITZYUK).handlePresenceUpdate(client, oldPresence, newPresence);
+    } else if (newPresence.guild.id === client.guildIdList.get('finnarmy')) {
+        (await guildFileImports.FINNARMY).handlePresenceUpdate(client, oldPresence, newPresence);
     }
 }
