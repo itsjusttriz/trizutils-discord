6d5973d200606e0949201d87dbb1e453a2a1bbc8
Triz <kiebusiness1@gmail.com>
2021-04-07 07:35:21 +0100

    Imported PublicStreamManager to finnarmy/events

--- DiscordCMCB/guilds/finnarmy/events.js
+++ DiscordCMCB/guilds/finnarmy/events.js
@@ -1,8 +1,15 @@
-import * as Discord from 'discord.js';
-import * as config from '../../config.js';
-import * as $ from '../../datapull/defaults.js';
-import { default as axios } from 'axios';
+import { PublicStreamManager } from "../../util/Public-TwitchStreamManager.js";
 
 const guildChannelMap = new Map([
-    ['mainLogChannel', '744698836701806592']
-])
+    ['mainLogChannel', '744698836701806592'],
+    ['publicLiveChannel', '587425956403085345']
+]);
+
+const guildRoleMap = new Map([
+    ['liveRole', '744402791396671509'],
+    ['approvedRole', '745724417887436861']
+]);
+
+export async function handlePresenceUpdate(client, oldPresence, newPresence) {
+    await PublicStreamManager.init(client, oldPresence, newPresence, guildChannelMap.get('publicLiveChannel'), guildRoleMap.get('approvedRole'), guildRoleMap.get('liveRole'));
+}
