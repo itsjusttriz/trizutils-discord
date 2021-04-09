ed3dd10448808486da0c9ce600c743071ae4678a
Triz <kiebusiness1@gmail.com>
2021-04-10 00:29:01 +0100

    Renamed Import based file

--- DiscordCMCB/datapull/guildFileImports.js
+++
@@ -1,8 +0,0 @@
-export default {
-    NIGHTSHADE_ALLEY: import('../guilds/nightshade_alley/events.js'),
-    CMCB_OFFICIAL: import('../guilds/cmcb_official/events.js'),
-    TOXICBUNCH: import('../guilds/toxicbunch/events.js'),
-    FINNARMY: import('../guilds/finnarmy/events.js'),
-    UNICORN_SHED: import('../guilds/unicorn_shed/events.js'),
-    BLITZYUK: import('../guilds/blitzyuk/events.js')
-}
