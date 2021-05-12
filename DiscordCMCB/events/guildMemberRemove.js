955ce5e42982baa2677af67d1dac6e17d4eff647
Triz <kiebusiness1@gmail.com>
2021-05-12 03:23:21 +0100

    Renamed GuildManager

--- DiscordCMCB/events/guildMemberRemove.js
+++ DiscordCMCB/events/guildMemberRemove.js
@@ -1,10 +1,10 @@
-import guildFileImports from '../datapull/guildFileImports.js';
+import { GuildManager } from '../util/ImportManager.js';
 
 export default async function (client, member) {
     if (member.guild.id === client.guildIdList.get('nightshade_alley')) {
-        (await guildFileImports.NIGHTSHADE_ALLEY).handleGuildMemberRemove(client, member);
+        (await GuildManager.NIGHTSHADE_ALLEY).handleGuildMemberRemove(client, member);
     } else if (member.guild.id === client.guildIdList.get('cmcb_official')) {
-        (await guildFileImports.CMCB_OFFICIAL).handleGuildMemberRemove(client, member);
+        (await GuildManager.CMCB_OFFICIAL).handleGuildMemberRemove(client, member);
     } else
         return;
 }
--- DiscordCMCB/events/guildMemberUpdate.js
+++ DiscordCMCB/events/guildMemberUpdate.js
@@ -1,10 +1,10 @@
-import guildFileImports from '../datapull/guildFileImports.js';
+import { GuildManager } from '../util/ImportManager.js';
 
 export default async function (client, oldMember, newMember) {
     if (oldMember.guild.id === client.guildIdList.get('nightshade_alley')) {
-        (await guildFileImports.NIGHTSHADE_ALLEY).handleGuildMemberUpdate(client, oldMember, newMember)
+        (await GuildManager.NIGHTSHADE_ALLEY).handleGuildMemberUpdate(client, oldMember, newMember)
     } else if (oldMember.guild.id === client.guildIdList.get('cmcb_official')) {
-        (await guildFileImports.CMCB_OFFICIAL).handleGuildMemberUpdate(client, oldMember, newMember);
+        (await GuildManager.CMCB_OFFICIAL).handleGuildMemberUpdate(client, oldMember, newMember);
     } else
         return;
 }
