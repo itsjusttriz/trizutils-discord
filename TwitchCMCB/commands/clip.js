f7417f440e8bd2ed1a7a8fe1ad703ad93c8c7679
Triz <kiebusiness1@gmail.com>
2021-05-12 03:29:32 +0100

    Update clip.js

--- TwitchCMCB/commands/clip.js
+++ TwitchCMCB/commands/clip.js
@@ -8,13 +8,13 @@ export default {
         const streamIsLive = await options.apiClient.helix.streams.getStreamByUserId(options.msg.channelId);
 
         if (streamIsLive) {
-            let clipId = await options.apiClient.helix.clips.createClip({ channelId: streamIsLive.id, createAfterDelay: true })
+            const clipId = await options.apiClient.helix.clips.createClip({ channelId: streamIsLive.userId, createAfterDelay: true });
 
             chatClient.say(options.channel, `NEW CLIP! https://clips.twitch.tv/${clipId} ~ by ${options.user}`);
         } else {
-            chatClient.say(options.channel, `${options.user}, You cannot create a clip for an offline channel.`)
+            chatClient.say(options.channel, `${options.user}, You cannot create a clip for an offline channel.`);
         }
 
         return chatClient.say(options.logChan, options.logMsg);
     }
-}
+};
