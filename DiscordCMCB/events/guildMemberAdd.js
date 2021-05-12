ff6ae919e4674ab8dce5409e200b59311e3ec161
Triz <kiebusiness1@gmail.com>
2021-05-12 03:23:00 +0100

    Renamed GuildManager

--- DiscordCMCB/events/guildMemberAdd.js
+++ DiscordCMCB/events/guildMemberAdd.js
@@ -1,10 +1,10 @@
-import guildFileImports from '../datapull/guildFileImports.js';
+import { GuildManager } from '../util/ImportManager.js';
 
 export default async function (client, member) {
     if (member.guild.id === client.guildIdList.get('nightshade_alley')) {
-        (await guildFileImports.NIGHTSHADE_ALLEY).handleGuildMemberAdd(client, member);
+        (await GuildManager.NIGHTSHADE_ALLEY).handleGuildMemberAdd(client, member);
     } else if (member.guild.id === client.guildIdList.get('cmcb_official')) {
-        (await guildFileImports.CMCB_OFFICIAL).handleGuildMemberAdd(client, member);
+        (await GuildManager.CMCB_OFFICIAL).handleGuildMemberAdd(client, member);
     } else
         return;
 }
