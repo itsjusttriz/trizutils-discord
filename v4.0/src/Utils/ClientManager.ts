ab9fa435cb054641564661c260aca9bdbef0f2e3
Triz <kiebusiness1@gmail.com>
2021-06-22 12:22:10 +0100

    Setup Twitch & Discord Clients

--- v4.0/src/Utils/ClientManager.ts
+++ v4.0/src/Utils/ClientManager.ts
@@ -1,19 +1,22 @@
-import * as TMI from 'tmi.js';
-import { ApiClient } from 'twitch';
+import * as TMIJS from 'tmi.js';
+// import { ApiClient } from 'twitch';
 
 import * as Discord from 'discord.js';
-// TODO: Import ApiClient.
 
 import ClientUtils from './ClientUtils.js';
 
 const TwitchClient = {
-    TMI: new TMI.Client(ClientUtils.Twitch.ClientOptions),
+    TMI: (env: NodeJS.ProcessEnv) => new TMIJS.Client(ClientUtils.Twitch.clientOpts(env)),
     // TODO: Setup ApiClient.
-    API: {}
+    API: {},
+    CommandMap: new Map()
 };
 
 // TODO: Setup Discord Client.
-const DiscordClient = new Discord.Client();
+const DiscordClient = {
+    SYS: new Discord.Client(),
+    CommandMap: new Map()
+}
 
 export default {
     TwitchClient,
