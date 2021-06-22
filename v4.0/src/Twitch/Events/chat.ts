ce7ad0dad597f6b80220a97e87c03a2a8cbf7843
Triz <kiebusiness1@gmail.com>
2021-06-22 12:26:00 +0100

    [Twitch] Created Basic setup for `CHAT` eventListener

--- v4.0/src/Twitch/Events/chat.ts
+++ v4.0/src/Twitch/Events/chat.ts
@@ -1,25 +1,54 @@
 import LoggerManager from "../../Utils/LoggerManager.js";
-import { ChatUserstate } from "tmi.js";
+import { ChatUserstate, Client } from "tmi.js";
+import ClientUtils from "../../Utils/ClientUtils.js";
+import ClientManager from "../../Utils/ClientManager.js";
 
-export default function (channel: string, userstate: ChatUserstate, message: string, self: boolean) {
+export default async function (TMIClient: Client, channel: string, userstate: ChatUserstate, message: string, self: boolean) {
+
+    // Split sentence into an array of words/args.
+    const args = message.slice(ClientUtils.Twitch.self.getPrefix.length).trim().split(/ +/g);
+
+    // Guarantee the 1st arg to be the 'command', and remove it from the previous array.
+    const command = args.shift()?.toLowerCase();
 
     const options = {
+        TMIClient,
+        APIClient: ClientManager.TwitchClient.API,
         channel,
         userstate,
         message,
         self,
+        args,
+        command,
+        isVip: userstate.badges?.vip,
         isSubOrFounder: userstate.subscriber || userstate.badges?.subscriber || userstate.badges?.founder,
         isMod: userstate.mod || userstate.badges?.moderator || userstate["user-type"] === 'mod',
         isCaster: userstate.badges?.broadcaster || userstate['room-id'] === userstate["user-id"],
-        isStaff: userstate.badges?.global_mod || userstate.badges?.admin || userstate.badges?.staff
+        isStaff: userstate.badges?.global_mod || userstate.badges?.admin || userstate.badges?.staff,
+        isBotAdmin: ClientUtils.Twitch.botAdmin.includes(userstate['username']!)
     };
 
-    // Log things.
-    LoggerManager.TwitchChatOptions.Log(options);
+    // Log messages to console.
+    LoggerManager.TwitchLogOptions.GenericMessageLog(options);
+
+    // TODO: Add BlacklistedTerms();
+    // TODO: Add ExtraCommands();
 
     // Don't listen to my own messages..
     if (self) return;
 
-    // Do your stuff.
+    // Ignore messages not starting with the prefix (in .env file)
+    if (!message.startsWith(ClientUtils.Twitch.self.getPrefix(process.env))) return;
 
+    // Define 'cmd'.
+    const cmd = await ClientManager.TwitchClient.CommandMap.get(command);
+
+    // If command is known, run it. Otherwise, ignore.
+    if (!cmd) return;
+
+    // If HandlerManager returns falsy, ignore.
+    // TODO: if (HandlerManager.cache['message']?.[channel] == false) return;
+
+    // If all previous null-checks have been passed, respond to command.
+    cmd.default.run(options);
 }
