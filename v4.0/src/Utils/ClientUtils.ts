8c1f2ffb964caa2e99f6edf6a8beae95c22e2d48
Triz <kiebusiness1@gmail.com>
2021-06-22 12:21:37 +0100

    Refactored to allow reading `.env` files

--- v4.0/src/Utils/ClientUtils.ts
+++ v4.0/src/Utils/ClientUtils.ts
@@ -1,29 +1,42 @@
 const Twitch = {
     self: {
-        getName: () => { return Twitch.ClientOptions.identity.username },
-        getPassword: () => { return Twitch.ClientOptions.identity.password }
+        getName: (env: NodeJS.ProcessEnv): string | undefined => { return env['TWITCH_USERNAME'] },
+        getPassword: (env: NodeJS.ProcessEnv): string | undefined => { return env['TWITCH_BASE_OAUTH'] },
+        getPrefix: (env: NodeJS.ProcessEnv): string => { return !env['TWITCH_DEV_MODE'] ? env['TWITCH_PREFIX']! : '..'; }
     },
-    ClientOptions: {
-        channels: ['nottriz'],
-        connection: { reconnect: true, secure: true },
-        identity: {
-            username: process.env.TWITCH_USERNAME,
-            password: process.env.TWITCH_BASE_OAUTH
-        },
-        options: {
-            messagesLogLevel: 'debug'
+    clientOpts: (env: NodeJS.ProcessEnv): any => {
+        const obj = {
+            connection: {
+                reconnect: true,
+                secure: true
+            },
+            channels: ['nottriz'],
+            identity: {
+                username: env['TWITCH_USERNAME'],
+                password: env['TWITCH_BASE_OAUTH']
+            },
+            options: { messagesLogLevel: 'debug' }
         }
+        return obj;
     },
-    tagColor: '#b700ff'
+    tagColor: '#b700ff',
+    debug: false,
+    botAdmin: ['itsjusttriz', 'nottriz', '47y_', 'tellik', 'rhilou32', 'immp']
 };
 
 // TODO: Add Content.
 const Discord = {
-    self: {}
+    self: {
+        getToken: (env: NodeJS.ProcessEnv): string => { return !env['DISCORD_DEV_MODE'] ? env['DISCORD_TOKEN']! : env['DISCORD_TEST_TOKEN']!; },
+        getPrefix: (env: NodeJS.ProcessEnv): string => { return !env['DISCORD_DEV_MODE'] ? env['PREFIX']! : '..'; }
+    },
+    tagColor: '#4946ab',
+    debug: true
 }
 
 //TODO: Add Content.
 const Twitter = {}
+
 
 export default {
     Twitch,
